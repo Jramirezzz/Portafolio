310367b46b4b2829d9a26ad5083735ea1ce21fa8
juan-esteban-ramirez <juanestebanrp123@gmail.com>
2025-02-06 10:21:16 -0500

    6

--- my-project/src/Components/CardsP/CardsP.jsx
+++ my-project/src/Components/CardsP/CardsP.jsx
@@ -2,7 +2,7 @@ import "./CardsP.css";
 
 export default function CardsP({ title, description, buttons, link }) {
     return (
-        <div className="cardp-container">
+        <div className="cardp-container floating">
             <h3> {title} </h3>
             <p> {description} </p>
             <a 
